840eff94c66fce03529d69a23f6040e83c03d249
pouyaardehkhani <apouyas120@gmail.com>
2024-06-09 17:28:38 +0330

    files

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from pytube import YouTube
 import os
-
-st.set_page_config(page_title="YouTube Video Downloader")
+import moviepy.editor as mp
 
 def fetch_streams(url):
     try:
@@ -34,9 +33,18 @@ def download_video(yt, video_quality, audio_quality, save_path):
         if audio_stream:
             st.info(f"Downloading audio: {audio_stream}")
             audio_file = audio_stream.download(output_path=save_path)
-            return video_file, audio_file
+            output_file = os.path.join(save_path, f"{yt.title}.mp4")
+            merge_audio_video(video_file, audio_file, output_file)
+            return output_file, None
     
     return video_file, None
+
+def merge_audio_video(video_file, audio_file, output_file):
+    video_clip = mp.VideoFileClip(video_file)
+    audio_clip = mp.AudioFileClip(audio_file)
+    
+    final_clip = video_clip.set_audio(audio_clip)
+    final_clip.write_videofile(output_file, codec='libx264', audio_codec='aac')
 
 st.title("YouTube Video Downloader")
 
@@ -45,23 +53,16 @@ if url:
     yt, video_qualities, audio_qualities = fetch_streams(url)
     if yt:
         video_quality = st.selectbox("Select video quality", video_qualities)
-        download_audio = st.checkbox("Download audio")
-        if download_audio:
-            audio_quality = st.selectbox("Select audio quality", audio_qualities)
-        else:
-            audio_quality = None
+        audio_quality = st.selectbox("Select audio quality", audio_qualities)
         save_path = "C:\Windows\System32\cmd.exe"
 
         if st.button("Download"):
             if not os.path.exists(save_path):
                 os.makedirs(save_path)
-            video_file, audio_file = download_video(yt, video_quality, audio_quality, save_path)
+            video_file, _ = download_video(yt, video_quality, audio_quality, save_path)
             if video_file:
                 st.success("Download complete!")
                 with open(video_file, "rb") as file:
                     st.download_button(label="Download Video", data=file, file_name=os.path.basename(video_file))
-                if audio_file:
-                    with open(audio_file, "rb") as file:
-                        st.download_button(label="Download Audio", data=file, file_name=os.path.basename(audio_file))
             else:
                 st.error("Error downloading the video. Please try again.")
